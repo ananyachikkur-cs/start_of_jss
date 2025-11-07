42f9b85bb05b111b06254c999c5715e5579b7cac
ananyachikkur-cs <ananyachikkur@gmail.com>
2025-11-07 21:14:22 +0530

    i am just learning

---
+++ index.js
@@ -0,0 +1,10 @@
+//switch option 
+const option = 2;
+
+switch (option) {
+    case 1: console.log("english");
+    break; // break is used to exit the switch statement they stop the moving forward 
+    case 2: console.log("spanish");
+    break;// if we dont use
+    case 3: console.log("french");
+}
